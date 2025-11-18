18e4174d0345873e28c119c1e0971c5682df676f
yeab127 <yeabmarg@outlook.com>
2025-11-18 12:14:47 -0800

    editing the style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,33 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 
 
 export default function Home() {
   return ( 
-  <div>
-<p>hello world</p>
-      
+  <div className="flex flex-col gap-y-4" >
 
+<div>
+  <Button variant="elevated">im a button</Button>
+</div>
+<div>
+  <Input placeholder="enter your answer here"/>
+
+</div>
+<div>
+<Progress value={50}></Progress>
+</div>
+<div>
+  <Textarea placeholder="comments please"/>
+
+</div>
+<div>
+  <Checkbox></Checkbox>
+</div>
+      
     </div>
     )
     }
