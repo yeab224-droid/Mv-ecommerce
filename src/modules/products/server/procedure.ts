a86afc7dcf858a9e44a0d011cfed99d9feb2b657
yeab127 <yeabmarg@outlook.com>
2025-12-19 03:02:27 -0800

    product-filtering

--- src/modules/products/server/procedure.ts
+++ src/modules/products/server/procedure.ts
@@ -15,20 +15,23 @@ export const productsRouter = createTRPCRouter ({
   )
     .query(async ({ctx,input}) =>{
       const where :Where={};
-    if (input.minPrice){
+    if (input.minPrice&& input.maxPrice){
      where.price = {
-      ...where.price,
+        
+      greater_than_equal: input.minPrice,
+      less_than_equal: input.maxPrice
+     }
+    
+    } else if (input.minPrice){
+     where.price = {
       greater_than_equal: input.minPrice
      }
-
-      if (input.maxPrice){
+    }else if (input.maxPrice){
      where.price = {
-      ...where.price,
       greater_than_equal: input.maxPrice
      }
     }
-
-      }
+     
          if (input.category){
            const categoriesData = await ctx.db.find({
             collection: "categories",
