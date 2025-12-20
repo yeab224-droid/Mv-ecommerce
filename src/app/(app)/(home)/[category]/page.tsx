b2812537e6a9e8dee887e682b6b4d0ed340c9353
yeab127 <yeabmarg@outlook.com>
2025-12-20 10:17:34 -0800

    adding view-list and infinite loading

--- src/app/(app)/(home)/[category]/page.tsx
+++ src/app/(app)/(home)/[category]/page.tsx
@@ -1,12 +1,9 @@
-import { Suspense } from "react";
+
 import { SearchParams } from "nuqs/server";
-import { ProductList, ProductListSkeleton } from "@/modules/products/ui/components/product-list";
 import { getQueryClient,trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { ProductFilters } from "@/modules/products/ui/components/products-filters";
 import { loadProductFilters } from "@/modules/products/search-params";
-import { ProductSort } from "@/modules/products/ui/components/product-sort";
-
+import { ProductListView } from "@/modules/products/ui/components/views/product-list-view";
 
 interface props {
     params: Promise<{
@@ -19,32 +16,17 @@ const Page = async ({params,searchParams}:props) =>{
    const {category} = await params;
    const filters = await loadProductFilters(searchParams);
 
-   console.log(JSON.stringify(filters),"this ids from RSC")
+
 
    const queryclient = getQueryClient();
-   void queryclient.prefetchQuery(trpc.products.getMany.queryOptions(
-    {category}
-   ))
+   void queryclient.prefetchQuery(trpc.products.getMany.queryOptions({
+    category,
+    ...filters
+   }))
 
     return (
  <HydrationBoundary state={dehydrate(queryclient)}>
-     <div className="px-4 lg:px-12 py-8 flex flex-col gap-4">
-        <div className="flex flex-col lg:flex-row lg:items-center gap-y-2 lg:gap-y-0 justify-between">
-          <p className="text-2xl font-medium">Curated for you</p>
-         <ProductSort />
-         </div>
-    <div  className="grid grid-cols-1 lg:grid-cols-6 xl:grid-cols-8 gap-y-6 gap-x-12">
-      <div className="lg:col-span-2 xl:col-span-2">
-       <ProductFilters />
-       </div>
-           <div className="lg:col-span-4 xl:col-span-6">
-       <Suspense fallback={ <ProductListSkeleton/>}>
-         <ProductList category={category}/>
-       </Suspense>
-       </div>
-
-    </div>
-  </div>
+    <ProductListView category={category}/>
     </HydrationBoundary>
     );
 }
--- src/modules/products/server/procedure.ts
+++ src/modules/products/server/procedure.ts
@@ -1,14 +1,17 @@
 
 
-import { Category } from "@/payload-types";
+import { Category, Media } from "@/payload-types";
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 import {  Sort,Where } from "payload";
 import { z } from "zod";
 import { sortValues } from "../search-params";
+import { DEFAULT_LIMIT } from "@/constants";
 
 export const productsRouter = createTRPCRouter ({
     getMany:baseProcedure
     .input(z.object({
+      cursor:z.number().default(1),
+      limit:z.number().default(DEFAULT_LIMIT),
        category: z.string().nullable().optional(), 
        minPrice:z.string().nullable().optional(),
         maxPrice:z.string().nullable().optional() ,
@@ -91,8 +94,16 @@ export const productsRouter = createTRPCRouter ({
        depth: 1,
        where,
        sort,
+       page:input.cursor,
+      limit:input.limit,
       });
 
-       return data;
+       return  {
+        ...data,
+        docs: data.docs.map((doc) =>({
+          ...doc,
+           image:doc.image as Media | null ,
+        }))
+       };
     })
 })
